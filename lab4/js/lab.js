b89dc331226d3b3b00984b2b926aea5882b2fcd1
kelmahoney <kelmahoneya@gmail.com>
2022-02-08 22:44:19 -0800

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,15 @@
+// Summary: Google dion run is the game we used to pseudocode.
+// Description: Using the arrows move the dino up and down to avoid the obstaclecs.
+// @link   https://trex-runner.com/
+// @file   This files defines the pseudocode.
+// @author Kelly Mahoney
+// @since  2.8.22
+
+
+  //Disconnect wifi or go to trex-runner.com
+    //Go to google browser
+        //Press spacebar to begin game
+        //Use arrow keys to move up or down to avoid obstacles
+        //Don’t run into the obstacles
+      //Do not stop…EVER
+    //Get a fucking high score
