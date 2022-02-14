713eb4c07f41b350de1d8fc5669da824b6ad0d8f
kelmahoney <kelmahoneya@gmail.com>
2022-02-13 20:26:26 -0800

    lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+// Summary: To show information about my car and its age
+// Description: Using Javascript to show information about my car
+// @author Kelly Mahoney
+// @since  2.13.22
+
+// define variables
+var make = "Toyota";
+var model = "Prius";
+var color = "White";
+var year = 2012;
+
+// calculate
+
+var age = 2020 - year
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
