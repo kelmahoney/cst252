eb7a07d8d581fc0e2ce3fd507af02fae63ae40f9
kelmahoney <kelmahoneya@gmail.com>
2022-02-24 11:19:35 -0800

    lab8

---
+++ lab8/js/lab.js
@@ -0,0 +1,28 @@
+/*
+ * Author: Kelly Mahoney
+ * Created:   2.24.2022
+ */
+
+ function isEven(x){
+    return (x % 2 == 0);
+ }
+
+ //test Function
+ console.log("Is 1 even? ", isEven(1));
+ console.log("Is 2 even? ", isEven(2));
+
+ array = [10, 28, 8, 2, 92, 80]
+ console.log("My array", array);
+
+ var result = array.map(isEven);
+ console.log("Test of eveness of array:" result);
+
+ var result = array.map(function(x){
+      return x ** 0.3;
+ })
+
+ console.log("Squareroot of array:", result);
+
+ //output
+ document.writeln(" ",
+      sortUserName(), "</br>");
