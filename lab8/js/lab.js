6cf0d3eb31344a3a3ef7cb6a31e007f4436404be
kelmahoney <kelmahoneya@gmail.com>
2022-03-01 10:54:41 -0800

    lab8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -3,29 +3,27 @@
  * Created:   2.24.2022
  */
 
- var outputEl = document.getElementById("output");
 
- function isEven(x){
+
+// is it even funtion
+function isEven(x){
     return (x % 2 == 0);
  }
 
  //test Function
- console.log("Is 1 even? ", isEven(1));
- console.log("Is 2 even? ", isEven(2));
+console.log("Is 1 Even? ", isEven(1));
+console.log("Is 2 Even ", isEven(2));
 
- array = [10, 28, 8, 2, 92, 80]
- console.log("My array", array);
+array = [10, 28, 8, 2, 92, 80];
+console.log("My array", array);
 
- var result = array.map(isEven);
- console.log("Test of eveness of array:" result);
+var result = array.map(isEven);
 
- var result = array.map(function(x){
-      return x ** 0.3;
+
+var result = array.map(function(x){
+      return x % 2;
  })
 
- console.log("Squareroot of array:", result);
 
  //output
- var newEl = document.createElement("p");
- newEl.innerHTML = "Evenness of array:" + JSON.stringify(result);
- outputEl.appendChild(newEl);
+document.writeln("is it even" + result);
