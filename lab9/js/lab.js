ecba7cce69e44065437f5d214a3943bfcd87bc5a
kelmahoney <kelmahoneya@gmail.com>
2022-03-01 23:03:29 -0800

    lab9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -3,5 +3,16 @@
  * Created:   3.1.2022
  */
 
-var document.oneEl("button");
-var document.twoEl("button");
+
+var oneEl = document.getElementById("output");
+var twoEl = document.createElement("button");
+
+oneEl.appendChild(twoEl);
+
+twoEl.id = "button";
+
+twoEl.innerHTML = "CLICK ME?";
+
+twoEl.addEventListener("click", () => {
+      alert ("congratulations you clicked the button");
+});
