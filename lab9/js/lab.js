fc67c66a3d9ffb8a763545e8c89ac3fb9b043446
kelmahoney <kelmahoneya@gmail.com>
2022-03-01 23:07:27 -0800

    another one

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -3,16 +3,16 @@
  * Created:   3.1.2022
  */
 
-
+// assigned to variable
 var oneEl = document.getElementById("output");
 var twoEl = document.createElement("button");
-
+// the variable
 oneEl.appendChild(twoEl);
 
 twoEl.id = "button";
 
 twoEl.innerHTML = "CLICK ME?";
-
+//output
 twoEl.addEventListener("click", () => {
       alert ("congratulations you clicked the button");
 });
