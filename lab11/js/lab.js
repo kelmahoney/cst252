351b4ad8a597507d797ca3f16ee04ebfc615ee61
kelmahoney <kelmahoneya@gmail.com>
2022-03-08 23:29:14 -0800

    lab11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -3,3 +3,9 @@
  * Created: 3.8.2022
  * License: Public Domain
  */
+
+$(".button");
+$("#output").append("<button class=my-button>THIS IS MY BUTTON");
+$(".my-button").click(function(){
+    alert("congratulations you clicked the button");
+})
